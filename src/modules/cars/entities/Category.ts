ec2c3a97462271847a1d1f8e76a7e608a8dbe666
Reginaldo Motta <reginaldomcv@gmail.com>
2021-04-13 21:13:27 -0400

    refatorando o model

--- src/modules/cars/entities/Category.ts
+++ src/modules/cars/entities/Category.ts
@@ -1,8 +1,13 @@
+import { Entity, PrimaryColumn, Colum} from 'typeorm';
 import { v4 as uuidV4 } from 'uuid';
 
+@Entity("categories")
 class Category {
 
+    @PrimaryColumn()
     id?: string;
+
+    @Column()
     name: string;
     description: string;
     created_at: Date;
