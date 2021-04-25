3339289a156e7d930a2b8ed2e27c7ba5d98c3b8f
Reginaldo Motta <reginaldomcv@gmail.com>
2021-04-25 11:08:06 -0400

    instalando o tsyringe e aplicando no CreateCategory

--- src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
@@ -1,15 +1,16 @@
-import { Category } from "../../entities/Category";
+import { inject, injectable } from 'tsyringe';
 import { ICategoriesRepository } from "../../repositories/ICategoriesRepository";
-
 
 interface IRequest {
     name: string;
     description: string
 }
-
+@injectable()
 class CreateCategoryUseCase {
 
-    constructor(private categoriesRepository: ICategoriesRepository) {
+    constructor(
+        @inject("CategoriesRepository")
+        private categoriesRepository: ICategoriesRepository) {
 
     }
 
